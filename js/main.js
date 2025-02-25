d6c542f945e8b2c985dba455257357584ac5e951
JunmanChoi <junman9595@gmail.com>
2025-02-26 01:34:55 +0900

    fix: 의미 없는 setTimeout 제거

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 function showTopBar() {
   let country = 'France';
   let vat = 20;
-  setTimeout(() => {
-    document.querySelector(
-      'section.country-bar'
-    ).innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
-  }, 1000);
+  document.querySelector(
+    'section.country-bar'
+  ).innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
 }
 
 showTopBar();
